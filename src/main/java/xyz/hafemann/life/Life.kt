0d30f7246e735d4653c0d1db63af38f97207c8a4
SLH335 <114884726+SLH335@users.noreply.github.com>
2023-11-29 18:47:49 +0100

    Add basic project structure

---
+++ src/main/java/xyz/hafemann/life/Life.kt
@@ -0,0 +1,13 @@
+package xyz.hafemann.life
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Life : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
